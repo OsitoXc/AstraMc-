167672c7665e4751d09d316ad8ffdddb1fb63e06
zNyx <152056642+OsitoXc@users.noreply.github.com>
2024-12-16 02:25:49 -0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function launchMinecraft() {
-    // Copia la IP al portapapeles y muestra un mensaje
-    navigator.clipboard.writeText("play.astramc.xyz").then(() => {
-        alert("IP copiada: play.astramc.xyz\n¡Conéctate a AstraMc en Minecraft!");
-    });
-}
